11eb0b4c5be6c94956906d08f4b7d8d86479368a
Noja0041 <marijon@tanmar.de>
2025-02-07 21:05:08 +0100

    not checked main code for Geräuschesensor

---
+++ tries/try_Soundsensor/try_Soundsensor.ino
@@ -0,0 +1,38 @@
+// <<< Inizalizing <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Initalizing <<<//
+int analog_input = A0; // Analoger Ausgang des Sensors --> direkter Messwert 
+int digital_input = 3; // Digitaler Ausgang des Sensors --> hier Signal wenn Messwert über eingestelltem Schwellwert
+  
+// <<< setup <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< setup <<<//
+void setup () {
+  pinMode(analog_input, INPUT);
+  pinMode(digital_input, INPUT);
+  Serial.begin(9600); 
+  Serial.println("KY-037 Geräuscherkennung");
+}
+  
+// Das Programm liest die aktuellen Werte der Eingang-Pins
+// und gibt diese auf der seriellen Ausgabe aus
+
+// <<< loop <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< loop <<<//
+void loop () {
+  float analog_value;
+  int digital_value;
+    
+  //Aktuelle Werte werden ausgelesen, auf den Spannungswert konvertiert (=kalibriert?)...
+  analog_value = analogRead(analog_input) * (5.0 / 1023.0); 
+  digital_value = digitalRead(digital_input);
+    
+  //... und an dieser Stelle ausgegeben
+  Serial.print("Analoger Spannungswert: "); 
+  Serial.print(analog_value, 4);
+  Serial.print(" V, \t Grenzwert: ");
+  
+  if (digital_value == 1) {
+      Serial.println("erreicht");
+  }
+  else {
+      Serial.println("noch nicht erreicht");
+  }
+  Serial.println("----------------------------------------------------------------");
+  delay(1000);
+}
