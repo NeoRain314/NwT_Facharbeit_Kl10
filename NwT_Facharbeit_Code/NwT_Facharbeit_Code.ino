0ce38fefbe3e5d0b5c6905ed13bb545486811822
NeoRain314 <jasmin.dammertz@googlemail.com>
2025-04-15 09:03:03 +0200

    organised main code
    added comments for better overview and changed some variables names

--- NwT_Facharbeit_Code/NwT_Facharbeit_Code.ino
+++ NwT_Facharbeit_Code/NwT_Facharbeit_Code.ino
@@ -1,34 +1,47 @@
-/*
+/***************************************************************************************
+
 g_... ---> globale Var
 p --> pointer
-*/
 
+Benennung:
+  class: camelCase
+  functions: camelCase
+  variables: snake_case
+
+****************************************************************************************/
+
+
+// <<< Inizalizing <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Initalizing <<//
 unsigned long t1 = 0;
 unsigned long t2 = 0;
 
+// ... libraries ...................................................................................................................... libraries ... //
+
+// ... Display .......................................................................................................................... Display ... //
+
+// ... Menu Structure ............................................................................................................ Menu Structure ... //
 class AbstractMenu {
   public: // public --> im folgenden funktionen, die von außen aufgerufen werden können
   virtual void draw(){  //virtual --> abgeleitete Klassen können funktion überschreiben
     Serial.println("Ich bin ein Abstract Menu!");
   }
   
-  virtual void ok_pressed() {
+  virtual void okPressed() {
     Serial.println("ok not implemented");
   }
 
-  virtual void select_pressed() {
+  virtual void selectPressed() {
     Serial.println("select not implemented");
   }
 };
 
-AbstractMenu* g_pActiveMenu = 0;  // g --> globale Variable; p --> pointer
-
+AbstractMenu* g_pActiveMenu = 0;
 AbstractMenu* g_pTimerMenu = 0;
 AbstractMenu* g_pMainMenu = 0;
 
 
 
-const char* main_menu_entries[] = {"Timer Menu", "Punkt 2", "Punkt 3"};
+const char* main_menu_entries[] = {"Punkt 1", "Punkt 2", "Punkt 3"};
 
 class MainMenu : public AbstractMenu {
   int selected_index = 0;
@@ -47,23 +60,26 @@ class MainMenu : public AbstractMenu {
     Serial.println("");
   }
 
-  virtual void select_pressed() {
+  virtual void selectPressed() {
     selected_index = (selected_index + 1) % 3; //damit i nie größer 2
   }
 
-  virtual void ok_pressed(){
-    g_pActiveMenu = g_pTimerMenu;x
+  virtual void okPressed(){
+    if (selected_index == 0) g_pActiveMenu = g_pMainMenu;
+    if (selected_index == 1) g_pActiveMenu = g_pMainMenu;
+    if (selected_index == 2) g_pActiveMenu = g_pMainMenu;
   }
 };
 
 class TimerMenu : public AbstractMenu {
+  int selected_index = 0;
   public:
 
   virtual void draw(){
     Serial.println("Timer Menu!");
   }
 
-  virtual void ok_pressed(){
+  virtual void okPressed(){
     if (selected_index == 0) g_pActiveMenu = g_pMainMenu;
     if (selected_index == 1) g_pActiveMenu = g_pMainMenu;
     if (selected_index == 2) g_pActiveMenu = g_pMainMenu;
@@ -71,12 +87,7 @@ class TimerMenu : public AbstractMenu {
 };
 
 
-// <<< Inizalizing <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Initalizing <<//
-
-
-// <<< setup <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< setup <<//
-
-
+// <<< setup <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< setup <<//
 void setup() {
   Serial.begin(9600); //Serial Monitor
   g_pMainMenu = new MainMenu();
@@ -84,23 +95,23 @@ void setup() {
   g_pActiveMenu = g_pMainMenu; //--> g_pActiveMenu legt hier start Menü fest
 }
 
-// <<< Loop <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Loop <<//
+// <<< Loop <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< loop <<//
 void loop() {
   g_pActiveMenu->draw();
   delay(1000);
-  g_pActiveMenu->ok_pressed();
+  g_pActiveMenu->selectPressed();
 }
 
-// <<< sub functions <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< sub functions <<//
+// <<< sub functions <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< sub functions <<//
 void updateClock(){
   Serial.println(millis());
 }
 
 
-// Wecker .................................................................................................. Wecker //
+// ... Wecker ............................................................................................................................ Wecker ... //
 
-// Timer  .................................................................................................. Timer //
 
+// ... Timer .............................................................................................................................. Timer ... //
 
 /*if(millis() > t1+1000){
     t1 = millis();
