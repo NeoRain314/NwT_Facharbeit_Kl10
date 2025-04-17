4576a30139ce2fd8a94fb6290b3d9aad9d549cf7
Noja0041 <marijon@tanmar.de>
2025-04-17 19:24:08 +0200

    unfunctional try_alarm

---
+++ tries/try_alarm/try_alarm.ino
@@ -0,0 +1,117 @@
+
+
+/* verwendete Pins:
+2 select_button_pin 4 CLK
+3 ok_button_pin     5 DIO
+*/ 
+
+// <<< library <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< library <<< //
+#include <Wire.h>
+// RealTimeClock:
+#include <RTClib.h>
+#include <Adafruit_BusIO_Register.h>
+#include <Adafruit_GenericDevice.h>
+#include <Adafruit_I2CDevice.h>
+#include <Adafruit_I2CRegister.h>
+#include <Adafruit_SPIDevice.h>
+
+// 7SegmentDisplay:
+#include <TM1637Display.h>
+
+
+
+// <<< Inizalizing <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Initalizing <<//
+
+int ah; // var for hours (->alarmhour) (used to you select on what thime the alarm should go off)
+int am; // var for minutes (->alarmminute)
+
+int hour; // var for the hours being showed to know what time it is
+int minute; // var for the minutes -"-
+
+// 7SegmentDisplay:
+#define SEG_CLK_PIN 4
+#define SEG_DIO_PIN 5
+TM1637Display display(SEG_CLK_PIN, SEG_DIO_PIN);
+
+// RealTimeClock:
+RTC_DS3231 rtc; 
+
+// Buttons:
+#define SELECT_BUTTON_PIN 2 
+#define OK_BUTTON_PIN 3
+int select_button_interrupt = 0;
+int ok_button_interrupt = 0;
+
+// Piezo:
+#define PIEZO_PIN 8
+
+
+
+// <<< setup <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< setup <<//
+void setup() {
+  Serial.begin(9600);
+
+  pinMode (SELECT_BUTTON_PIN, INPUT_PULLUP);
+  attachInterrupt (digitalPinToInterrupt(SELECT_BUTTON_PIN), selectButtonISR, RISING );
+  pinMode (OK_BUTTON_PIN, INPUT_PULLUP);
+  attachInterrupt (digitalPinToInterrupt(OK_BUTTON_PIN), okButtonISR, RISING); //Rising 0 -> 1
+  
+  pinMode (PIEZO_PIN, OUTPUT);
+
+  if (!rtc.begin()) {
+    Serial.println("RTC nicht gefunden!");
+    while (1);
+ }
+
+ // Falls die RTC nicht eingestellt ist, die aktuelle Zeit setzen:
+  if (rtc.lostPower()) {
+    Serial.println("RTC hat Strom verloren, Setze Zeit auf Kompilierzeit!");
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
+
+  display.setBrightness(0x0f); // Helligkeit vom Display einstellen (15)
+}
+
+
+
+// <<< Loop <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< loop <<//
+void loop() {
+
+  showTime();
+
+ /*if (select_button_interrupt == true){ 
+  ah ++;
+ }
+ 
+ if (ok_button_interrupt == true){
+ }
+ */
+ if (ah == hour && am == minute){
+  tone(PIEZO_PIN, 400);
+ }
+}
+// <<< sub functions <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< sub functions <<//
+void showTime(){
+    DateTime now = rtc.now();
+
+  Serial.print(now.hour());
+    Serial.print(":");
+    Serial.println(now.minute());
+    hour = now.hour();
+    minute = now.minute();
+
+  hour = (hour + 1) % 24; // Sommerzeit (+1)
+ display.showNumberDecEx(hour * 100 + minute, 0b01000000);
+
+
+}
+// ... Interrupts .................................................................................................................... Interrupts ... //
+void okButtonISR() {
+  ok_button_interrupt = true;
+}
+
+void selectButtonISR() {
+  select_button_interrupt = true;
+}
+
+ 
