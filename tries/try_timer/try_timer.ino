b9ef580ec92d578ad1b748a79e4ab5e0a6981ae5
NeoRain314 <jasmin.dammertz@googlemail.com>
2025-04-18 10:15:39 +0200

    finished try_timer code

--- tries/try_timer/try_timer.ino
+++ tries/try_timer/try_timer.ino
@@ -1,22 +1,46 @@
-int timer_time = 10;
+int timer_time[] = {0, 10};
+unsigned long timer_start_mill = 0;
+unsigned long timer_time_mill = 0;
+
+bool timer_stat = true;
 
 void setup() {
-  Serial.Begin(9600);
+  Serial.begin(9600);
+  Serial.println("");
 
+  timer_time_mill = timer_time[0]*60000 + timer_time[1]*1000;
+  timer_start_mill = millis();
+  Serial.println(timer_time_mill);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if(timer_stat) timer();
 }
 
+void timer() {
+  if(millis() > timer_start_mill + timer_time_mill) {
+    timer_stat = false;
+    Serial.println("");
+    Serial.println("-----------------------");
+    Serial.println("Timer ended!");
+    return;
+  }
 
-void printTimerTime() {
-  int minutes = totalSeconds / 60;
-  int seconds = totalSeconds % 60;
+  unsigned long rest_mill = timer_start_mill + timer_time_mill - millis();
+  int minutes = rest_mill/60000;
+  int seconds = (rest_mill%60000) /1000;
 
-  // Anzeige im Format MM:SS mit führender Null
-  char buffer[6];
-  sprintf(buffer, "%02d:%02d", minutes, seconds);
-  Serial.println(buffer);
+  //Print Timer on Serial Monitor
+  Serial.print("Timer: ");
+  Serial.print(intToString(minutes, true));
+  Serial.print(":");
+  Serial.println(intToString(seconds, true));
+}
+
+char* intToString(int num, bool leading_zero) {
+  static char buffer[16]; // 2 Stellen + Nullterminierung
+
+  if (leading_zero) sprintf(buffer, "%02d", num); 
+  else sprintf(buffer, "%d", num);
+  return buffer;
 }
