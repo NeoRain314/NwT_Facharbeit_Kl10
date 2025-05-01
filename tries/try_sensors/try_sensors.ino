7776c0bb5f414b2ae6e17a20738dd064494147b9
NeoRain314 <jasmin.dammertz@googlemail.com>
2025-05-01 11:53:37 +0200

    added new file for all sensors combinded

---
+++ tries/try_sensors/try_sensors.ino
@@ -0,0 +1,19 @@
+// <<< inizaializing <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< inizaializing <<< //
+#define MOTIONSENSOR_PIN 7
+
+int motion_stat = 0; // darin wird später gespeichert ob eine Bewegung erkannt wurde oder nicht 
+
+// <<< setup <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< setup <<< //
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(MOTIONSENSOR_PIN, INPUT);
+}
+
+// <<< loop <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< loop <<< //
+
+void loop() {
+  motion_stat = digitalRead(MOTIONSENSOR_PIN); // wenn 5 Volt (also Bewegung wahrgenommen) dann HIGH; wenn 0 Volt (also keine Bewegung) dann LOW --> wird in Variable bewegungsstatus gespeichert
+  Serial.println(motion_stat);
+  delay(500);
+}
