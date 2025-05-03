b530721bd5d977c00de8d8cf1f3ed39be7900209
NeoRain314 <jasmin.dammertz@googlemail.com>
2025-05-03 17:10:15 +0200

    work in progress study mode

--- NwT_Facharbeit_Code/NwT_Facharbeit_Code.ino
+++ NwT_Facharbeit_Code/NwT_Facharbeit_Code.ino
@@ -281,12 +281,19 @@ class TimerMenu : public AbstractMenu {
 };
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Study Menu ~~~ //
-const char* study_menu_entries[] = {"...", "...", "...", "back"};
+const char* study_menu_entries[] = {"25/5 min", "Rept: 1 ", "start", "back"};
+const char* study_menu_modes[] = {"25/5 min", "50/10", "90/20 min"};
+int studytimer_time[] = {0, 0};
 
 class StudyMenu : public AbstractMenu {
   int selected_index = 0;
   
   public:
+  bool study_stat = 0;
+  int study_mode = 0;
+  int study_repetitions = 1;
+  unsigned long studytimer_star_mill = 0;
+  unsigned long studytimer_time_mill = 0;
 
   virtual void draw(){
     printMenuBar("Study Menu");
@@ -299,8 +306,21 @@ class StudyMenu : public AbstractMenu {
 
   virtual void okPressed(){
     beforeMenuSwitch();
-    if (selected_index == 0) g_pActiveMenu = g_pStudyMenu;
-    if (selected_index == 1) g_pActiveMenu = g_pStudyMenu;
+    if(study_stat == true) {
+      //interrupt study mode
+      study_stat = false;
+    }
+    
+    if (selected_index == 0){
+      study_mode = (study_mode + 1) % 3; 
+      study_menu_entries[0] = study_menu_modes[study_mode];
+    }
+    if (selected_index == 1){
+      study_repetitions += 1;
+      if(study_repetitions>5) study_repetitions = 1;
+
+      sprintf(study_menu_entries[1], "Rept: %d", study_repetitions);
+    }
     if (selected_index == 2) g_pActiveMenu = g_pStudyMenu;
     if (selected_index == 3) g_pActiveMenu = g_pMainMenu;
   }
