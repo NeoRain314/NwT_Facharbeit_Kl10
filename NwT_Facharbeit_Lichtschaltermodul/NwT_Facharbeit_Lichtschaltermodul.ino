f10a891a2312b7350ca49121e2f7a3fcf6b584d6
Noja0041 <marijon@tanmar.de>
2025-05-14 22:03:24 +0200

    complaining lichtschatermodul for reciver

--- NwT_Facharbeit_Lichtschaltermodul/NwT_Facharbeit_Lichtschaltermodul.ino
+++ NwT_Facharbeit_Lichtschaltermodul/NwT_Facharbeit_Lichtschaltermodul.ino
@@ -1,51 +1,31 @@
-#include <Wire.h>
-
-#include <VirtualWire.h>
-
-// <<< library <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< library <<< //
 #include <Servo.h>
-#include <Arduino.h>
 
-// <<< inizaializing <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< inizaializing <<< //
-#define RX_PIN 11  // Pin für den Empfänger
-#define SERVO_PIN 9  // Pin für den Servo
+#include <RCSwitch.h>
 
-int test = true
+RCSwitch mySwitch = RCSwitch();
 
 Servo S1;
 
-// <<< setup <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< setup <<< //
 void setup() {
   Serial.begin(9600);
-
-  S1.attach(3); 
-  vw_set_rx_pin(RX_PIN);
-  vw_setup(2000); // Funkfrequenz (2000 Baud)
-  vw_rx_start(); // Start des Empfängers
+  mySwitch.enableReceive(0);  // Pin für Empfänger
+  S1.attach(9);
 }
 
-// <<< loop <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< loop <<< //
 void loop() {
-  uint8_t buffer[10];  // Puffer für empfangene Daten
-  uint8_t buflen = sizeof(buffer);
-
- /* if (vw_get_message(buffer, &buflen)) {  //wenn ein Signal empfangen wird
-    Serial.println("Signal empfangen! Schalter wird betätigt.");
-    activateLight(); 
-*/  
-
-    if (test == true){
-      test = false;
-      activateLight(); 
-
+  if (mySwitch.available()) {
+    int receivedValue = mySwitch.getReceivedValue();
+    Serial.println(receivedValue);
+    if (receivedValue == 1234) { 
+      tunServo();
     }
-
+    mySwitch.resetAvailable();
   }
+}
 
-}
-void activateLight() {
-  Serial.println("Schalter betätigen!");
-  myServo.write(90);  // Servo auf Position bringen
-  delay(1000); //!! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! 
-  myServo.write(0);    // Zurücksetzen
-}
+
+void tunServo(){
+  S1.write(90);  // Servo auf Position setzen
+  delay(1000);
+  S1.write(0);
+  }
