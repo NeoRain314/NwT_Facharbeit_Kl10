902e0c45bf0fdd0a46d96941f9871854994f0e26
NeoRain314 <jasmin.dammertz@googlemail.com>
2025-06-30 22:09:29 +0200

    update bluetooth try

--- tries/try_bluetooth/try_bluetooth.ino
+++ tries/try_bluetooth/try_bluetooth.ino
@@ -1,37 +1,28 @@
-#include <SoftwareSerial.h>
-
 #define ledPin 7
 int state = 0;
 
 void setup() {
   pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, HIGH);
   Serial.begin(9600); // Default communication rate of the Bluetooth module
-
-  
-
-  /*digitalWrite(ledPin, HIGH);
-  delay(1000);
-  digitalWrite(ledPin, LOW);*/
 }
 
 void loop() {
-  
-  //Serial.println("Hallo");
-  //delay(1000);
-
-  Serial.println(Serial.available());
-  if(Serial.available() > 0){ // Checks whether data is comming from the serial port
+ if(Serial.available() > 0){ // Checks whether data is comming from the serial port
     state = Serial.read(); // Reads the data from the serial port
  }
 
- if (state == 0) {
+ if (state == '0') {
   digitalWrite(ledPin, LOW); // Turn LED OFF
-  //Serial.println("LED: OFF"); // Send back, to the phone, the String "LED: ON"
+  Serial.println("LED: OFF"); // Send back, to the phone, the String "LED: ON"
   state = 0;
  }
- else if (state == 1) {
+ else if (state == '1') {
   digitalWrite(ledPin, HIGH);
-  //Serial.println("LED: ON");;
+  Serial.println("LED: ON");;
   state = 0;
  } 
+
+ Serial.print("Testtext :D 12345 !!");
+ delay(1000);
 }
--- tries/try_bluetooth2_sendandrecieve/try_bluetooth2_sendandrecieve.ino
+++ tries/try_bluetooth2_sendandrecieve/try_bluetooth2_sendandrecieve.ino
@@ -1,5 +1,42 @@
+#include <RTClib.h>
+
 #define ledPin 7
 int state = 0;
+
+
+int soundValues[20] = {
+  45, 52, 48, 55, 60, 58, 53, 47, 50, 49,
+  54, 57, 56, 62, 59, 61, 63, 55, 52, 50
+};
+
+DateTime timeStamps[20] = {
+  DateTime(2025, 6, 30, 9, 0, 0),
+  DateTime(2025, 6, 30, 9, 5, 0),
+  DateTime(2025, 6, 30, 9, 10, 0),
+  DateTime(2025, 6, 30, 9, 15, 0),
+  DateTime(2025, 6, 30, 9, 20, 0),
+  DateTime(2025, 6, 30, 9, 25, 0),
+  DateTime(2025, 6, 30, 9, 30, 0),
+  DateTime(2025, 6, 30, 9, 35, 0),
+  DateTime(2025, 6, 30, 9, 40, 0),
+  DateTime(2025, 6, 30, 9, 45, 0),
+  DateTime(2025, 6, 30, 9, 50, 0),
+  DateTime(2025, 6, 30, 9, 55, 0),
+  DateTime(2025, 6, 30, 10, 0, 0),
+  DateTime(2025, 6, 30, 10, 5, 0),
+  DateTime(2025, 6, 30, 10, 10, 0),
+  DateTime(2025, 6, 30, 10, 15, 0),
+  DateTime(2025, 6, 30, 10, 20, 0),
+  DateTime(2025, 6, 30, 10, 25, 0),
+  DateTime(2025, 6, 30, 10, 30, 0),
+  DateTime(2025, 6, 30, 10, 35, 0)
+};
+
+int movementValues[20] = {
+  3, 7, 5, 6, 4, 8, 7, 6, 5, 4,
+  7, 9, 8, 6, 5, 4, 3, 7, 6, 5
+};
+
 
 void setup() {
   pinMode(ledPin, OUTPUT);
@@ -8,21 +45,14 @@ void setup() {
 }
 
 void loop() {
- if(Serial.available() > 0){ // Checks whether data is comming from the serial port
-    state = Serial.read(); // Reads the data from the serial port
- }
+  /*Serial.print("Testtext :D 12345 !!");
+  delay(1000);*/
 
- if (state == '0') {
-  digitalWrite(ledPin, LOW); // Turn LED OFF
-  Serial.println("LED: OFF"); // Send back, to the phone, the String "LED: ON"
-  state = 0;
- }
- else if (state == '1') {
-  digitalWrite(ledPin, HIGH);
-  Serial.println("LED: ON");;
-  state = 0;
- } 
-
- Serial.print("Testtext :D 12345 !!");
- delay(1000);
+  for (int i = 0; i < 100; i++) {
+    Serial.print(soundValues[i]);
+    Serial.print(",");
+    Serial.print(movementValues[i]);
+    Serial.print(",");
+    Serial.println(timeStamps[i].unixtime()); // oder .second()/.minute() usw.
+  }
 }
